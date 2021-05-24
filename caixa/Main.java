8c031e64c0bc9d0d8cd5564a6721015835f85d97
JPGaeta <jpgaeta.dev@gmail.com>
2021-05-24 12:34:01 -0300

    feat Colher CPF

---
+++ caixa/Main.java
@@ -0,0 +1,24 @@
+package caixa;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println("============Seja bem vindo===========");
+        System.out.println("=====================================");
+
+
+        Scanner ler = new Scanner(System.in);
+        String cpf;
+
+        System.out.println("Informe seu cpf: ");
+        cpf = ler.nextLine();
+
+        if ("123.456.789-00".equals(cpf)) {
+
+        }
+    }
+}
